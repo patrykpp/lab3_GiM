b196ca6c5d3c1e8cf582ed79df811b074604603d
patrykpp <patryk583@vp.pl>
2018-04-11 21:00:06 +0200

    lab3_GiM
    Grafika i Multimedia lab3

---
+++ Assets/Scripts/Pyramid.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pyramid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        int maxHeight = 10;
+        SpringJoint spring;
+        GameObject gameObject;
+
+        for (int height = 0; height < maxHeight; height++)
+        {
+            int length = maxHeight - height;
+            for (int x = -length; x <= length; x++)
+            {
+                for (int z = -length; z <= length; z++)
+                {
+                    if (Mathf.Abs(x) == length || Mathf.Abs(z) == length)
+                    {
+                        gameObject = Tools.MakeCapsule(x, height, z);
+                        spring = gameObject.AddComponent<SpringJoint>();
+
+                    }
+
+                }
+                
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
